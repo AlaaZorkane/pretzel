f20a8564631b2942cfb760fa948c86ebbbdecad6
AlaaZorkane <raralazoril@gmail.com>
2018-03-15 13:41:26 +0000

    updated semantic and gulp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './Loader.css';
 import registerServiceWorker from './registerServiceWorker';
 import Loader from './Loader';
 import semantic from 'semantic-ui-react'
-import './semantic/dist/semantic.css';
+import './semantic/dist/src';
 
 
 ReactDOM.render(<Loader />, document.getElementById('root'));
