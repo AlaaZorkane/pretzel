96191263c77bf4b6a6c8be68410dcceeb59f649f
AlaaZorkane <raralazoril@gmail.com>
2018-03-18 19:24:42 +0000

    updated the layout ( in/out functions are working )

--- src/Loader.js
+++ src/Loader.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import './Loader.css';
+import ReactDOM from 'react-dom'
 
 const Pretzel = () => (
     <div className="loaderMain">
         <div className="background">
-            <h1 className="logo">Pretzel</h1>
+            <h2 className="logo" id="salt">Elegant, fast, and with 0 salt.</h2>
+            <h1 className="logo" id="logo" onMouseLeave={out} onMouseEnter={enter}>Pretzel!</h1>
         </div>
     </div>
 )
+
+function out() {
+    console.log('OUT')
+}
+function enter() {
+    console.log('enter')
+}
 
 export default Pretzel;
